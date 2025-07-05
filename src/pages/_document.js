415a4850527e024b7b66348e626cecb4b42806b9
Muhammet <muhammet075@icloud.com>
2025-07-06 01:39:11 +0300

    change

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,16 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="nl">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-K6F5E7W6ED"></script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZXYZRXRCVR"></script>
       <script
         dangerouslySetInnerHTML={{
           __html: `window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', 'G-K6F5E7W6ED');`
+          gtag('config', 'G-ZXYZRXRCVR');`
         }}
       />
       </Head>
